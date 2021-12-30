5369a60f8ac89e6765a7a005143cc82845b7672e
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2021-12-30 02:37:24 +0100

    remove react-native-svg leftovers

--- metro.config.js
+++ metro.config.js
@@ -18,11 +18,9 @@ module.exports = async () => {
           inlineRequires: true,
         },
       }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg', 'cjs'],
+      sourceExts: [...sourceExts, 'cjs'],
     },
   }
 }

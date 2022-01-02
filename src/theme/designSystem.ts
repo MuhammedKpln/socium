c7c74e8947cad08ec8af95bac92fbfceaa97095c
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-01-02 21:22:59 +0100

    use theme design colors

--- src/theme/designSystem.ts
+++ src/theme/designSystem.ts
@@ -1,30 +1,25 @@
+import { Platform } from 'react-native'
 import { Colors, Typography } from 'react-native-ui-lib'
 
 const colors = {
-  primary: '#5383b8', // blue
-  secondary: '#469c57', // green
-  accent: '#fed330', // yellow
-  blackish: Colors.rgba(20, 20, 20, 1),
-  blackish2: Colors.rgba(50, 50, 50, 1),
-  whitish: Colors.rgba(250, 250, 250, 1),
-  whitish2: Colors.rgba(230, 230, 230, 1),
-  info: '#5383b8',
-  error: Colors.red40,
-  success: Colors.green40,
+  primary: '#FE7949',
+  surfaceDark: '#1C1C1C',
+  surfaceLight: '#F8F8F8',
+  textDark: '#fff',
+  textLight: '#000',
+  greyText: '#999999',
 }
 
 const themes = {
   light: {
-    textColor: colors.blackish,
-    screenBG: colors.whitish,
-    surfaceBG: colors.whitish2,
-    bg2Color: colors.whitish2,
+    textColor: colors.textLight,
+    screenBG: '#000',
+    surfaceBG: colors.surfaceLight,
   },
   dark: {
-    textColor: colors.whitish,
-    screenBG: colors.blackish,
-    surfaceBG: colors.blackish2,
-    bg2Color: colors.blackish2,
+    textColor: colors.textDark,
+    screenBG: '#000',
+    surfaceBG: colors.surfaceDark,
   },
 }
 
@@ -35,5 +30,27 @@ export const configureDesignSystem = (): void => {
 
   Typography.loadTypographies({
     section: { fontSize: 26, fontWeight: '600' },
+    header: Platform.select({
+      android: {
+        fontSize: 22,
+        fontWeight: '700',
+        fontFamily: 'SourceSansPro-Bold',
+      },
+      ios: {
+        fontSize: 22,
+        fontWeight: '700',
+      },
+    }),
+    title: Platform.select({
+      android: {
+        fontSize: 17,
+        fontWeight: '700',
+        fontFamily: 'SourceSansPro-Bold',
+      },
+      ios: {
+        fontSize: 22,
+        fontWeight: '700',
+      },
+    }),
   })
 }

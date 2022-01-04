a5b0f4dac4de80519287316a730aa338bf4a191d
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-01-04 13:52:15 +0100

    enable screens and freeze

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
+import { enableFreeze, enableScreens } from 'react-native-screens'
+
+enableScreens(true)
+enableFreeze(true)
 
 AppRegistry.registerComponent(appName, () => App)

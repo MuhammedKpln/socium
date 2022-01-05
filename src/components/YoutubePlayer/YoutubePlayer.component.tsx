17afda187965d7e1fd1e51cfd2cc8718874afc23
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-01-05 21:00:39 +0100

    skeleton view for youtube thumbnail

--- src/components/YoutubePlayer/YoutubePlayer.component.tsx
+++ src/components/YoutubePlayer/YoutubePlayer.component.tsx
@@ -1,17 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { TouchableOpacity } from 'react-native'
-import FastImage from 'react-native-fast-image'
+import { Image, TouchableOpacity } from 'react-native'
+import SkeletonView from 'react-native-ui-lib/skeletonView'
+import View from 'react-native-ui-lib/view'
 import YoutubePlayer, { getYoutubeMeta } from 'react-native-youtube-iframe'
-
-export interface IYTPlayerProps {
-  videoId: string
-}
+import { IYTPlayerProps } from './YoutubePlayer.props'
 
 export function YTPlayer(props: IYTPlayerProps) {
   const { videoId } = props
   const [thumbnailUrl, setThumbnailUrl] = useState<string>('')
   const [renderYoutubeFrame, setRenderYoutubeFrame] = useState<boolean>(false)
-
+  const [showContent, setShowContent] = useState<boolean>(false)
   const fetchYoutubeMeta = useCallback(async () => {
     const youtubeMeta = await getYoutubeMeta(videoId)
 
@@ -47,21 +45,42 @@ export function YTPlayer(props: IYTPlayerProps) {
     fetchYoutubeMeta()
   }, [fetchYoutubeMeta])
 
+  useEffect(() => {
+    Image.prefetch(thumbnailUrl).then(val => {
+      if (val) {
+        setShowContent(true)
+      }
+    })
+  }, [thumbnailUrl])
+
+  const renderContent = useCallback(() => {
+    return (
+      <Image
+        source={{ uri: thumbnailUrl }}
+        style={{
+          width: 150,
+          height: 100,
+          borderRadius: 4,
+          marginTop: 20,
+          marginRight: 10,
+        }}
+      />
+    )
+  }, [thumbnailUrl])
+
   return (
     <TouchableOpacity onPress={() => setRenderYoutubeFrame(true)}>
       {renderYoutubeFrame ? (
         renderYoutube()
       ) : (
-        <FastImage
-          source={{ uri: thumbnailUrl }}
-          style={{
-            width: 150,
-            height: 100,
-            borderRadius: 4,
-            marginTop: 20,
-            marginRight: 10,
-          }}
-        />
+        <View marginT-20>
+          <SkeletonView
+            width={150}
+            height={100}
+            showContent={showContent}
+            renderContent={renderContent}
+          />
+        </View>
       )}
     </TouchableOpacity>
   )
---
+++ src/components/YoutubePlayer/YoutubePlayer.props.ts
@@ -0,0 +1,3 @@
+export interface IYTPlayerProps {
+  videoId: string
+}

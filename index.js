ec61c4446d37c672c0715c3d2fc4a57a3e677877
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-01-15 10:42:04 +0100

    gesturehandler

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 import { enableFreeze, enableScreens } from 'react-native-screens'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 enableScreens(true)
 enableFreeze(true)
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App))

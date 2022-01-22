82056298aa4dda7ecaf05c70c5778a0f1a4cd0ba
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-01-22 11:25:37 +0100

    animate match screen

--- src/containers/Match/Match.container.tsx
+++ src/containers/Match/Match.container.tsx
@@ -1,6 +1,11 @@
 import { Page } from '@/components/Page/Page.component'
 import { wait } from '@/utils/utils'
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
 import Text from 'react-native-ui-lib/text'
 import View from 'react-native-ui-lib/view'
 import { MatchComponent } from './components/Match.component'
@@ -9,34 +14,67 @@ import { MatchingFoundComponent } from './components/MatchingFound.component'
 export function MatchContainer() {
   const [matching, setMatching] = useState<boolean>(false)
   const [matched, setMatched] = useState<boolean>(false)
+  const marginBottom = useSharedValue(0)
+  const opacity = useSharedValue(1)
+
+  const containerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      marginBottom: withTiming(marginBottom.value, {
+        duration: 400,
+      }),
+    }
+  })
+  const matchComponentStyle = useAnimatedStyle(() => {
+    return {
+      opacity: withTiming(opacity.value, {
+        duration: 600,
+      }),
+    }
+  })
+
+  const onPressMatch = useCallback(() => {
+    marginBottom.value = 300
+    opacity.value = 0
+
+    wait(700).then(() => {
+      setMatching(true)
+    })
+  }, [marginBottom, opacity])
 
   useEffect(() => {
     if (matching) {
       wait(2000).then(() => {
         setMatched(true)
+        marginBottom.value = 0
+
+        opacity.value = 1
       })
     }
-  }, [matching])
+  }, [matching, marginBottom, opacity])
 
   return (
-    <Page center>
-      {!matching ? (
-        <MatchComponent onPressMatch={() => setMatching(!matching)} />
-      ) : (
-        <View>
-          <Text fontGilroy marginT-250 style={{ fontSize: 22 }}>
-            Eşleşme bekleniyor...
-          </Text>
-          {matched ? (
-            <MatchingFoundComponent
-              onPressClose={() => {
-                setMatching(false)
-                setMatched(false)
-              }}
-            />
-          ) : null}
-        </View>
-      )}
+    <Page animated center>
+      <Animated.View style={containerAnimatedStyle}>
+        {!matching ? (
+          <Animated.View style={matchComponentStyle}>
+            <MatchComponent onPressMatch={() => onPressMatch()} />
+          </Animated.View>
+        ) : (
+          <View>
+            <Text fontGilroy marginT-250 style={{ fontSize: 22 }}>
+              Eşleşme bekleniyor...
+            </Text>
+            {matched ? (
+              <MatchingFoundComponent
+                onPressClose={() => {
+                  setMatching(false)
+                  setMatched(false)
+                }}
+              />
+            ) : null}
+          </View>
+        )}
+      </Animated.View>
     </Page>
   )
 }

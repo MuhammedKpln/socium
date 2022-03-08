068e636c9dd16ad8a339378048bcc809d0adc386
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-03-08 12:32:09 +0100

    remove unnecesary imports

--- src/containers/Profile/Profile.container.tsx
+++ src/containers/Profile/Profile.container.tsx
@@ -48,7 +48,6 @@ import { useLazyQuery, useMutation, useQuery } from '@apollo/client'
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import React, { useCallback, useLayoutEffect, useMemo, useState } from 'react'
 import { Platform } from 'react-native'
-import FastImage from 'react-native-fast-image'
 import {
   Colors,
   DateTimePicker,
